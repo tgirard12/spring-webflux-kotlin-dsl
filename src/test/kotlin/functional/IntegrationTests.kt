75041a5af99dc1621f5e5c241d9b7150ed1f3521
Thomas Girard <thomas.girard12@gmail.com>
2017-09-25 13:46:08 +0200

    Refactor main app with dsl

--- src/test/kotlin/functional/IntegrationTests.kt
+++ src/test/kotlin/functional/IntegrationTests.kt
@@ -3,6 +3,7 @@ package functional
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.http.MediaType.*
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
@@ -10,59 +11,58 @@ import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.test.test
 import java.time.LocalDate
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IntegrationTests {
-	
-	private val application = Application(8181)
-	private val client = WebClient.create("http://localhost:8181")
-	
-	@BeforeAll
-	fun beforeAll() {
-		application.start()
-	}
-	
-	@Test
-	fun `Find all users on JSON REST endpoint`() {
-		client.get().uri("/api/users")
-				.accept(APPLICATION_JSON)
-				.retrieve()
-				.bodyToFlux<User>()
-				.test()
-				.expectNextMatches { it.firstName == "Foo" && it.lastName == "Foo" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Bar" && it.lastName == "Bar" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Baz" && it.lastName == "Baz" && it.birthDate.isBefore(LocalDate.now()) }
-				.verifyComplete()
-	}
+    private val client = WebClient.create("http://localhost:8181")
 
-	@Test
-	fun `Find all users on HTML page`() {
-		client.get().uri("/users")
-				.accept(TEXT_HTML)
-				.retrieve()
-				.bodyToMono<String>()
-				.test()
-				.expectNextMatches { it.contains("Foo") && it.contains("Bar") && it.contains("Baz") }
-				.verifyComplete()
-	}
+    @BeforeAll
+    fun beforeAll() {
+        application.start(8181)
+    }
 
-	@Test
-	fun `Receive a stream of users via Server-Sent-Events`() {
-		client.get().uri("/api/users")
-				.accept(TEXT_EVENT_STREAM)
-				.retrieve()
-				.bodyToFlux<User>()
-				.test()
-				.expectNextMatches { it.firstName == "Foo" && it.lastName == "Foo" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Bar" && it.lastName == "Bar" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Baz" && it.lastName == "Baz" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Foo" && it.lastName == "Foo" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Bar" && it.lastName == "Bar" && it.birthDate.isBefore(LocalDate.now()) }
-				.expectNextMatches { it.firstName == "Baz" && it.lastName == "Baz" && it.birthDate.isBefore(LocalDate.now()) }
-				.thenCancel()
-				.verify()
-	}
-	
-	@AfterAll
-	fun afterAll() {
-		application.stop()
-	}
+    @Test
+    fun `Find all users on JSON REST endpoint`() {
+        client.get().uri("/api/users")
+                .accept(APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux<User>()
+                .test()
+                .expectNextMatches { it.firstName == "Foo" && it.lastName == "Foo" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Bar" && it.lastName == "Bar" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Baz" && it.lastName == "Baz" && it.birthDate.isBefore(LocalDate.now()) }
+                .verifyComplete()
+    }
+
+    @Test
+    fun `Find all users on HTML page`() {
+        client.get().uri("/users")
+                .accept(TEXT_HTML)
+                .retrieve()
+                .bodyToMono<String>()
+                .test()
+                .expectNextMatches { it.contains("Foo") && it.contains("Bar") && it.contains("Baz") }
+                .verifyComplete()
+    }
+
+    @Test
+    fun `Receive a stream of users via Server-Sent-Events`() {
+        client.get().uri("/api/users")
+                .accept(TEXT_EVENT_STREAM)
+                .retrieve()
+                .bodyToFlux<User>()
+                .test()
+                .expectNextMatches { it.firstName == "Foo" && it.lastName == "Foo" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Bar" && it.lastName == "Bar" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Baz" && it.lastName == "Baz" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Foo" && it.lastName == "Foo" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Bar" && it.lastName == "Bar" && it.birthDate.isBefore(LocalDate.now()) }
+                .expectNextMatches { it.firstName == "Baz" && it.lastName == "Baz" && it.birthDate.isBefore(LocalDate.now()) }
+                .thenCancel()
+                .verify()
+    }
+
+    @AfterAll
+    fun afterAll() {
+        application.stop()
+    }
 }
